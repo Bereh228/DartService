5c02271ac2ab1bd05008d8288af9c9e887d61de9
Bereh228 <andrey.berezhko999@gmail.com>
2021-05-19 12:26:45 +0300

    add second carousel and section

--- DartService/js/script.js
+++ DartService/js/script.js
@@ -1,33 +1,82 @@
 const dropDownMenuItems = document.querySelectorAll('.favor__element-item');
 
-// // for(let item of dropDownMenuItems){
-// //     item.addEventListener('click',function(){
-// //         item.classList.toggle('favor__element-item_active');
-// //     });
-// // }
+for(let item of dropDownMenuItems){
+    item.addEventListener('click',function(){
+        item.classList.toggle('favor__element-item_active');
+    });
+}
 
 
-function checkAddClass(item){
-     for(let value of item){
-         value.classList.remove('favor__element-item_active');
-     }
+// function checkAddClass(item){
+//     for(let value of item){
+//         value.classList.remove('favor__element-item_active');
+//     }
 
-     for(let value of item){
-         value.addEventListener('click',function(){
-             value.classList.toggle('favor__element-item_active');
-         });
-     }
- }
+//     for(let value of item){
+//         value.addEventListener('click',function(){
+//         value.classList.toggle('favor__element-item_active');
+//     });
+//   }
+// }
 
- checkAddClass(dropDownMenuItems);
+//   checkAddClass(dropDownMenuItems);
+
+
+// let divWrapper = document.querySelector('.favor__element');
+// let divElement = document.querySelectorAll('.favor__element-item');
+
+// divWrapper = addEventListener('click', (event)=>{
+// let div = event.target;
+// div.classList.add('favor__element-item_active');
+// for(let item of divElement){
+//   console.log(item)
+//   if(item.classList.contains('favor__element-item_active') && item!==event.target)
+//   item.classList.remove('favor__element-item_active');
+// }
+// })
+
 
  $('.about__carousel').slick({
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
     arrows: true,
-    vertical: true
+    vertical: true,
+    verticalSwipping: true,
+    swipe: true,
+    waitForAnimate: false,
+    
   });
+
+  $('.team__carousel').slick({
+    centerMode: true,
+    centerPadding: '10px',
+    slidesToShow: 3,
+    arrows: false,
+    touchThreshold: 100,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
 
 // let selectedTd;
 
